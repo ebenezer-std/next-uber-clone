780db28af10b400113e85205d73425e41a825917
Ebenezer <eaanertey@st.ug.edu.gh>
2021-11-25 14:25:45 +0000

    Implemented Map Component

---
+++ pages/components/Map.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useEffect } from 'react'
+
+import tw from "tailwind-styled-components"
+import mapboxgl from '!mapbox-gl'
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYmVuYWJzdGVjdCIsImEiOiJja3dkdTVxcjExZzlwMnVxbDk2cW53MWZ2In0.DsvBFKDQsFkqmrMHa_M9pw';
+
+
+const Map = () => {
+
+    
+  useEffect(() => {
+
+const map = new mapboxgl.Map({
+container: "map",
+style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
+center: [-99.29011, 39.39172],
+zoom: 5
+});
+    
+  })
+  // only runs the first time the application is loaded
+
+
+
+
+    return (
+        <Wrapper id="map">
+
+        </Wrapper>
+    )
+}
+
+const Wrapper = tw.div`
+ flex-1 
+
+`
+export default Map
