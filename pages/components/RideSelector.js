dc2fc550fdfb735f86378fac67be3cce5835c0bd
Ebenezer <eaanertey@st.ug.edu.gh>
2021-12-13 14:31:05 +0000

    Optimized for deployment

--- pages/components/RideSelector.js
+++ pages/components/RideSelector.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import tw from "tailwind-styled-components";
-import { carList } from "../data/CarList";
+import { carList } from "../../CarList";
 
 const RideSelector = ({ pickupCoordinates, dropOffCoordinates }) => {
   const [rideDuration, setRideDuration] = useState(0);
